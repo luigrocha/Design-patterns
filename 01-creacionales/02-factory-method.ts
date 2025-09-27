a9f5fe8711036ba4ff8af88309ed1379d51e51be
lgrocha <luigrocha@gmail.com>
2025-09-27 13:16:15 -0300

    feat(factory-method): implement factory method pattern

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,87 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Hamburger {
+    prepare(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+
+    prepare(): void {
+        console.log('Preparing %cChicken Hamburger', COLORS.yellow);
+    }
+}
+
+class BeefHamburger implements Hamburger {
+
+    prepare(): void {
+        console.log('Preparing %cBeef Hamburger', COLORS.red);
+    }
+}
+
+class BeanHamburger implements Hamburger {
+
+    prepare(): void {
+        console.log('Preparing %cBean Hamburger', COLORS.blue);
+    }
+}
+
+abstract class Restaurant {
+    abstract createHamburger(): Hamburger;
+
+
+    orderHamburguer(): void {
+        const hamburger = this.createHamburger();
+        hamburger.prepare();
+    }
+}
+
+
+class ChickenRestaurant extends Restaurant {
+
+    override createHamburger(): Hamburger {
+        return new ChickenHamburger();
+    }
+}
+
+class BeefRestaurant extends Restaurant {
+
+    override createHamburger(): Hamburger {
+        return new BeefHamburger();
+    }
+}
+
+class BeanRestaurant extends Restaurant {
+
+    override createHamburger(): Hamburger {
+        return new BeanHamburger();
+    }
+}
+function main() {
+    let restaurant: Restaurant;
+
+    const burgerType = prompt(`What type of burger do hoy want? (chicken/beef/bean)`)
+
+    switch (burgerType) {
+        case 'chicken':
+            restaurant = new ChickenRestaurant();
+            break;
+
+        case 'beef':
+            restaurant = new BeefRestaurant();
+            break;
+        case 'bean':
+            restaurant = new BeanRestaurant();
+            break;
+        default:
+            throw new Error('Invalid option')
+            
+    }
+
+    restaurant.orderHamburguer();
+}
+
+main();
